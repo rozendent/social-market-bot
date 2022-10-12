1df47381fcdb2f6fba14fd511c53d7cac7b5f110
rozendent <rozasasha@icloud.com>
2022-10-12 22:18:13 +0300

    .

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import Message
 from aiogram.utils import executor
 
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
-from aiogram import Bot, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-import json
-import logging
-
-import os
-
-
 from data import db_session
 from data.users import User
 from keyboards import *
@@ -24,41 +12,6 @@ from setup import texts, dp, session, bot
 from tools import make_user_message, get_relevant_user, get_empty_spheres_dict, get_list_from_spheres_dict
 
 poll_storage = dict()
-
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-TOKEN = '1084737102:AAGSfZBZ_KVb_BAW2QyMHPt0a2PmopsOWuM'
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
-
 
 
 class Form(StatesGroup):
